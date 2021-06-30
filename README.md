559754711fe158b5cddb9e27ba0dffc9327f0410
shiponcs <admatinm@gmail.com>
2021-06-30 21:25:35 +0600

    update README.md: compilation and run command

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # tcp-server-client-cpp
+
+- To compile and run server
+
+    `g++ TCP_socket.cpp server_main.cpp -o server
+    ./server` 
+- To compile and run client
+    `g++ TCP_socket.cpp server_client.cpp -o client
+    ./client` 
--- client_main.cpp
+++ client_main.cpp
@@ -1,13 +1,25 @@
 #include "TCP_socket.hpp"
 #include <iostream>
 
-int main() {
+int main()
+{
   TCPSocketSpace::TCPSocketClient client_socket;
-  client_socket.connet("157.230.41.161", 33333);
-  client_socket.send("I'm in\n");
-  client_socket.receive();
-  char buffer[1024] = {0};
-  client_socket.send(buffer);
+  client_socket.connet("36.255.68.139", 5222);
+    client_socket.send("<?xml version='1.0'?> <stream:stream to='ckotha.com' xmlns='jabber:client' xmlns:stream='http://etherx.jabber.org/streams' version='1.0'>");
+    client_socket.receive();
+    client_socket.receive();
+    std::cout << "******************\n";
+    client_socket.send("<iq type='set' id='auth2'><query xmlns='jabber:iq:auth'><username>matin1</username><password>safeai</password><resource>jingle</resource></query></iq>");
+    client_socket.receive(); 
+    // client_socket.send("<message from='matin1@ckotha.com' to='matin2@ckotha.com' xml:lang='en'>");
+    // client_socket.send("<body>Hello</body>");
+    // client_socket.send("</message>");
+    client_socket.receive();
+    client_socket.receive();
+    client_socket.receive();
+    // client_socket.send("</stream:stream>");
+  // char buffer[1024] = {0};
+  // client_socket.send(buffer);
 
   return 0;
 }
